d2eb09e47a11db2d72a817b98c21a7ed4a12c6b1
Tony Stark <apurificato5680@gmai.com>
2024-04-06 17:39:03 -0400

    fixed javascript code so that website now functions to save blog posts using local storage and JSON, and it outputs appended data to the html and displays in browser

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,108 +1,54 @@
 // Select the ul from the DOM
-const blogsOutput = document.querySelector('ul')
-
-// Select all delete buttons from the DOM
-const deleteBtns = document.querySelectorAll('.delete-btn')
-
-console.log(deleteBtns)
+const blogsOutput = document.querySelector('ul');
 
 // Grab the blog data from localStorage
-const raw = localStorage.getItem('blogs')
-let blogs = JSON.parse(raw) || []
+const raw = localStorage.getItem('blogs');
+const blogs = JSON.parse(raw) || [];
 
 // Recieve the delete button element
 function removeBlog(btnObj) {
-    // Get the parent element of the button
-    const parent = btnObj.parentElement 
-    // Get the text of the button (used to retrieve the name)
-    const para = parent.querySelector('p')
-    const name = para.innerText
-    // use the array filter method to filter the friends array out and remove the name matching the button text
+    const parent = btnObj.parentElement;
+    const para = parent.querySelector('.title');
+    const title = para.innerText;
+
     const filtered = blogs.filter(function (blog) {
-        if (blog !== name) {
-            return true
-        }
-    })
+        return blog.title !== title;
+    });
 
-    blogs = filtered
+    localStorage.setItem('blogs', JSON.stringify(filtered));
 
-    localStorage.setItem('blogs', JSON.stringify(blogs))
+    parent.remove();
 
-    // Overwrite the old names data in localstorage(stringify)
-    // localStorage.removeItem('friends')
-    // Remove the button's parent <li> from the DOM
-    parent.remove()
-
-    //If friends is empty, add the paragraph to our ul that says "no friends have been added"
-    if (!blogs.length) {
-        blogsOutput.innerHTML = '<p>No blog posts have been added yet.</p>'
+    if (!filtered.length) {
+        blogsOutput.innerHTML = '<p>No blog posts have been added yet.</p>';
     }
 }
 
-// function removeFriend(eventObj) {
-//     console.log(eventObj.target.parentNode)
-
-//     eventObj.target.parentNode.remove()
-// }
-
-
-
-// Create a function that loops over the names array and outputs an li to the ul for each name in the names array
+// Create a function that loops over the blogs array and outputs an li to the ul for each title in the blogs array
 function outputBlogs() {
-    // If friends array is not empty, remove the no friends paragraph from the DOM
-    if (blogs.length){
-        blogsOutput.innerHTML = ''
+    if (blogs.length) {
+        blogsOutput.innerHTML = '';
     }
 
     for (let blog of blogs) {
-        // Insert HTML into the friendsOutput Ul at the inside end (beforeend)
         blogsOutput.insertAdjacentHTML('beforeend', `
-        <li class="row align-center">
-        <p>${blog}</p>
-        <button class="delete-btn">Delete</button>
-        </li>
-        `
-        //remove onclick="removeFriend(event" from the code above
-        )
+            <li class="row align-center">
+                <p class="title">${blog.title}</p>
+                <p class="content">${blog.content}</p>
+                <p class="username">Post written by ${blog.username}</p>
+                <button class="delete-btn">Delete</button>
+            </li>
+        `);
     }
 }
 
-outputBlogs()
+outputBlogs();
 
-// Create an eventListener on all delete buttons so when clicked they remove that name from localstorage and also remove the li from the window.
-
+// Create an eventListener on all delete buttons so when clicked they remove that blog from localstorage and also remove the li from the window.
 document.body.addEventListener('click', function (eventObj) {
-    const el = eventObj.target
-    // console.log(el.classList.contains("delete-btn"))
-    
-    // check if the element that was clicked was the delete button (use the classList to see if the element has a class of delete-btn).
+    const el = eventObj.target;
+
     if (el.classList.contains('delete-btn')) {
-    // when a delete button is clicked, pass the element to the remove friend function.
-        removeBlog(el)
+        removeBlog(el);
     }
-})
-
-// const fruits = ['orange', 'apple', 'grape']
-
-// fruits.forEach(function (item) {
-//     console.log(item)
-// })
-
-// forEach won't allow you to break out of loop; it goes over every single item in the array and passes the index for each. forEach is a shorter version of a for loop
-
-// for (let fruit of fruits) {
-//     console.log(fruit)
-
-//     if (fruit === 'apple') {
-//         break;
-//     }
-// }
-// standard for and for of loop, you can break out after a point (same for while loop)
-
-// const filteredArr = fruits.filter(function (fruit) {
-//     if (fruit !=='apple') {
-//         return true
-//     }
-// })
-// // this removes the item from the array, doesn't allow it to display, filters it out, displays the others.
-// console.log(filteredArr)
+});
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,64 +1,33 @@
 // Select the form element
+const blogForm = document.querySelector('#blog-form');
 
-const blogForm = document.querySelector('#blog-form')
+function addBlogs(eventObj) {
+    eventObj.preventDefault();
 
-/**
-    Create a function that triggers when the submit event occurs
+    const usernameValue = blogForm.username.value;
+    const titleValue = blogForm.postTitle.value;
+    const contentValue = blogForm.postText.value;
 
-        - Stop/Prevent the page from refreshing
-        - Grab the input value from our name input (what they typed into the box)
-        - Grab all the names that have already been stored to local storage
-        - Add the new name to the names
-        - Overwrite the old names value in local storage with the newly updated names
-        - Send the user to the View Friends page
- */
+    const raw = localStorage.getItem('blogs');
+    const blogs = JSON.parse(raw) || [];
 
-function addBlog(eventObj) {
-    //- Stop/Prevent the page from refreshing
-    eventObj.preventDefault()
+    const blog = {
+        username: usernameValue,
+        title: titleValue,
+        content: contentValue
+    };
 
-    //- Grab the input value from our name input (what they typed into the box)
-    const usernameValue = blogForm.username.value
-    console.log(usernameValue)
-    const titleValue = blogForm.postTitle.value
-    console.log(titleValue)
-    const textValue = blogForm.postText.value
-    console.log(textValue)
+    blogs.push(blog);
 
-    // - Grab all the names that have already been stored to local storage
-    const raw = localStorage.getItem('blogs')
-    const blogs = JSON.parse(raw) || []
+    localStorage.setItem('blogs', JSON.stringify(blogs));
 
-    console.log(blogs)
-
-    //- Add the new name to the names
-    blogs.push(usernameValue, titleValue, textValue)
-    // names.push(textValue)
-
-    // - Overwrite the old names value in local storage with the newly updated names
-    localStorage.setItem('blogs', JSON.stringify(blogs))
-
-    // - Send the user to the View Friends page
-    window.location = 'blog.html'
+    // Redirect to blog.html
+    window.location.href = 'blog.html';
 }
-
 
 // Create a submit event listener for the form
-
-// Wrap all initial starting listeners and any code that needs to run when the page loads in a function(initializing function)
-
 function init() {
-    blogForm.addEventListener('submit', addBlog)
-    // Create a submit event listener for the form
+    blogForm.addEventListener('submit', addBlogs);
 }
 
-init()
-
-// localStorage.setItem('num', 10)
-// localStorage.setItem('bool', true)
-
-// console.log(bool === 'true' && true)
-
-
-
-// when working with local storage, when you store an array, you stringify it and then when you are retrieving the array you parse (both of these using JSON). Examples are: JSON.stringify() and JSON.parse()
+init();
